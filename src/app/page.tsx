74d1d462fe148c7fdcce67da544a633c8184d012
kanamaru2525 <mfkanamaru@gmail.com>
2024-04-11 16:43:13 +0900

    heroviewの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
+import Header from "@/component/header";
 import Navi from "@/component/Navi";
 import { Box, Button, Flex,Text } from "@yamada-ui/react";
-import Link from "next/link";
+import { Noto_Sans_JP } from 'next/font/google'
+
+const notojp = Noto_Sans_JP({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  variable: "--font-notojp",
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <Box _scrollbar={{display:"none"}}>
-        <Flex  justifyContent="flex-end" position="fixed" w="100%">
-          <Navi href="/portforio"title="About"></Navi>
-          <Navi href="/portforio"title="Works"></Navi>
-          <Navi href="/portforio"title="Contact"></Navi>
-        </Flex>
-      <Flex h="100vh" bg="#B9C0CC" justifyContent="center" alignItems="center" display="flex">   
-          <Text fontSize="128px">Kanamaru</Text>
-          <Text fontSize="128px">Taisei</Text>
+    <Box _scrollbar={{display:"none"}} fontFamily="notojp">
+      <Header></Header>
+      <Flex h="100vh" bg="#B9C0CC" justifyContent="center" alignItems="center" flexDirection="column" display="flex">   
+          <Text fontSize="96px"fontWeight="black"color="white" borderBottom="solid">KANAMARU TAISEI</Text>
+          <Text fontSize="32px" color="white" margin="25px">A failure to become a creator</Text>
       </Flex>
       <Box h="100vh">
-      
+        
       </Box>     
     </Box>
   )
