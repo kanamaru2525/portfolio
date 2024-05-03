890ba4c76bdfb76941f006e1dde8d6cd6a13cbef
kanamaru2525 <mfkanamaru@gmail.com>
2024-05-03 22:12:27 +0900

    フッター

---
+++ src/component/footer.tsx
@@ -0,0 +1,11 @@
+import { Center } from "@yamada-ui/react"
+
+const Footer = () => {
+    return (
+        <Center h="5vh" bg="black">
+            <Center color="white"justifyContent="center">🄫KanamaruTaisei-2024</Center>
+       </Center>
+    )
+  }
+  
+  export default Footer
