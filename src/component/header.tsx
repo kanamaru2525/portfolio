3bb4088923d74a6deaa6110256c8eedb07619675
kanamaru2525 <mfkanamaru@gmail.com>
2024-05-03 22:12:56 +0900

    ヘッダーインポート変更

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
     return (
     <Flex  justifyContent="flex-end" position="fixed" w="100%">
         <Navi href="/"title="About"></Navi>
-        <Navi href="/portforio"title="Works"></Navi>
-        <Navi href="/portforio"title="Contact"></Navi>
+        <Navi href="/portfolio"title="Works"></Navi>
+        <Navi href="/portfolio"title="Contact"></Navi>
     </Flex>
     )
   }
