8d3663c64e82cbca64dd841a3220b5954ecfb1db
kanamaru2525 <mfkanamaru@gmail.com>
2024-06-14 01:16:03 +0900

    アーキテクチャを書いた

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -29,13 +29,16 @@ export default function Portfolio() {
             </WorksContents>
         </GridItem>        
         <GridItem w="full" h="45vh"  bg="#D9D9D9" >
-        <WorksContents title="市役所発表向けポスターの作成" pic="/images/poster-PD.png" product_time="2023年11月頃1週間" article="記事を書く" />
+        <WorksContents title="市役所発表向けポスターの作成" pic="/images/poster-PD.png" product_time="2023年11月頃:1週間" 
+        article="市役所と協力し、地域の課題解決を行う授業の中で解決策発表用に作成したポスター$n青色は知的なイメージがあるため、青を基調しアクセントカラーとして黄色を用いた。上から順序経って結論まで提示するようなデザインにした。" />
         </GridItem>
         <GridItem w="full" h="45vh"  bg="#D9D9D9" >
-          <WorksContents title="プロジェクト新人歓迎ポスター作成" pic="/images/poster-project.png" product_time="2022年" article="記事を書く" />
+          <WorksContents title="プロジェクト新人歓迎ポスター作成" pic="/images/poster-project.png" product_time="2024年3月:3日" 
+          article="4月に学内プロジェクトの合同説明会があるということで、プロジェクトより作成を依頼されたポスター$nプロジェクトの意向によって、明るいイメージを出すため、赤を基調とした。サークル用に作成したポスターの評判が良かったため、構図を再利用した。" />
         </GridItem>    
         <GridItem w="full" h="45vh"  bg="#D9D9D9" >
-          <WorksContents title="プロジェクト新人歓迎動画作成" pic="/images/MV.png" product_time="2022年" article="記事を書く$nhttps://youtu.be/gtCcKbqwJx8?t=193" />
+          <WorksContents title="プロジェクト新人歓迎動画作成" pic="/images/MV.png" product_time="2024年2月:1か月" 
+          article="https://youtu.be/gtCcKbqwJx8?t=193 $n 学校より、プロジェクトの紹介動画を作成せよとの通達があったために作成。DaVinci Resolveをこの時まで触ったことがなかったが、動画作成方法を覚えた。また、この動画は見やすいように話者が話した内容を要約した字幕を付けた。" />
         </GridItem>
       </Grid>
       </Center>
