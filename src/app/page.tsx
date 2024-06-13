ec53fb98223f49874aa684fe13d223ba81d06563
kanamaru2525 <mfkanamaru@gmail.com>
2024-06-13 23:48:29 +0900

    画像の追加とかSkillsの追加　あと最適化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import Footer from "@/component/footer";
 import Header from "@/component/header";
-import { Box, Flex,Text,Image, List, ListItem, Motion, Center } from "@yamada-ui/react";
+import { Box, Flex,Text,Image, List, ListItem, Motion, Center, Grid, GridItem } from "@yamada-ui/react";
 import { Noto_Sans_JP } from 'next/font/google'
 
 const notojp = Noto_Sans_JP({
@@ -31,15 +31,56 @@ export default function Home() {
             <List fontSize="40px"marginRight="25px"marginLeft="25px"paddingTop="5" >
               <ListItem>Name:金丸汰生</ListItem>
               <ListItem>Age:20</ListItem>
-              <ListItem>Hobby:TRPG・自炊</ListItem>
+              <ListItem>Hobby:TRPG・アナログゲーム</ListItem>
               <ListItem>From:Awaji</ListItem>
             </List>
           </Flex>
         </Box>
-        <Box h="100vh" bg="#2F4A78">
-        <Box h="10vh"justifyContent="center" alignItems="center"display="flex">
-        <Text marginRight="25px"marginLeft="25px"paddingTop="5"fontSize="32px"fontWeight="black"color={"white"}>Skill</Text>
-      </Box>
+          <Box h="full" bg="#2F4A78">
+            <Box h="10vh"justifyContent="center" alignItems="center"display="flex">
+            <Text marginRight="25px"marginLeft="25px"paddingTop="5"fontSize="32px"fontWeight="black"color={"white"}>Skill</Text>
+            </Box>
+          <Box>
+          <Center>
+            <Grid templateColumns="repeat(2, 1fr)" gap={"md"} margin="5vh" w="65vw">
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/figma.png" alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%" textAlign="center">Figma</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:★★★</Text>
+              </GridItem>
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/htmls.png"  alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%" textAlign="center">HTMLL/Javascript/CSS</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:★★</Text>
+              </GridItem>
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/Nextjs.png"  alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%"textAlign="center">Next.js</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:★</Text>
+              </GridItem>
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/daVinci.png"  alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%"textAlign="center">DaVinci Resolve</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:★★</Text>
+              </GridItem>
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/colors.png"  alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%"textAlign="center">色彩検定</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:2級</Text>
+              </GridItem>
+            </Grid>
+          </Center>
+        </Box>  
         </Box>
         <Footer></Footer>  
     </Box>
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -13,7 +13,7 @@ export default function Portfolio() {
   return (
     <div>
       <Box _scrollbar={{display:"none"}} fontFamily="notojp">
-      <Header></Header>
+      <Header />
       <Flex h="50vh" bg="#B9C0CC" justifyContent="center" alignItems="center" flexDirection="column" display="flex"> 
           <Text fontSize="96px"fontWeight="black"color="white" borderBottom="solid">
             Works
@@ -22,21 +22,26 @@ export default function Portfolio() {
       <Center>
       <Grid templateColumns="repeat(2, 1fr)" gap={"md"} margin="5vh" w="65vw">
 
-        <GridItem w="full" h="45vh"  bg="#D9D9D9">
-            <WorksContents title="テスト" pic="/images/me.jpg" article="記事を書く"></WorksContents>
-          </GridItem>        
-        <GridItem w="full" h="45vh"  bg="#D9D9D9">
-          <WorksContents title="テスト" pic="/images/me.jpg" article="記事を書く"></WorksContents>
+        <GridItem w="full" h="45vh"  bg="#D9D9D9" >
+            <WorksContents title="サークル新人歓迎ポスター作成" pic="/images/poster-TRPG.png" product_time="2023年3月頃:1週間" 
+            article="4月頭の部活動・サークル活動紹介オリエンテーション用に作成したポスター$n世間一般のTRPGのイメージであるクトゥルフ神話TRPGのメインカラーが緑なので、大きく緑色を使用した。視点の方向を意識し、Z方向に視線を誘導するように文字を配置した。">
+
+            </WorksContents>
+        </GridItem>        
+        <GridItem w="full" h="45vh"  bg="#D9D9D9" >
+        <WorksContents title="市役所発表向けポスターの作成" pic="/images/poster-PD.png" product_time="2023年11月頃1週間" article="記事を書く" />
         </GridItem>
-        <GridItem w="full" h="45vh"  bg="#D9D9D9"></GridItem>
-        <GridItem w="full" h="45vh"  bg="#D9D9D9"></GridItem>
-        <GridItem w="full" h="45vh"  bg="#D9D9D9"></GridItem>
-        <GridItem w="full" h="45vh"  bg="#D9D9D9"></GridItem>
+        <GridItem w="full" h="45vh"  bg="#D9D9D9" >
+          <WorksContents title="プロジェクト新人歓迎ポスター作成" pic="/images/poster-project.png" product_time="2022年" article="記事を書く" />
+        </GridItem>    
+        <GridItem w="full" h="45vh"  bg="#D9D9D9" >
+          <WorksContents title="プロジェクト新人歓迎動画作成" pic="/images/MV.png" product_time="2022年" article="記事を書く$nhttps://youtu.be/gtCcKbqwJx8?t=193" />
+        </GridItem>
       </Grid>
       </Center>
       
       </Box>
-      <Footer></Footer>
+      <Footer />
     </div>
   )
   };
